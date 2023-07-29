0df67da7e5308a7fe69a8014a40ae850a5418bc9
gabriellneeves7 <138191283+gabriellneeves7@users.noreply.github.com>
2023-07-28 23:04:11 -0400

    criando somar de numeros

--- Algoritmos/main.js
+++ Algoritmos/main.js
@@ -1,1 +1,8 @@
 // Aqui é o principal, onde tevem ser importadas e chamadas todos os algoritmos de acordo com a documentação.
+
+import somarNumeros from "./somaDeNumeros.js";
+//import encontrarMaiorNumero from "./encontraMaiorNumero";
+
+
+const numero = [1,2,3,4,5];
+console.log(somarNumeros(numero));
