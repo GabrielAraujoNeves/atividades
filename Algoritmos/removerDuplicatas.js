73a54e70aefcd3114be99fa82921b42b680c84eb
GabrielAraujoNeves <138191283+gabriellneeves7@users.noreply.github.com>
2023-08-09 20:52:32 -0400

    Foi criar o teste para a função removerDuplicatas.js

--- Algoritmos/removerDuplicatas.js
+++ Algoritmos/removerDuplicatas.js
@@ -4,6 +4,8 @@ function removerDuplicadas(arr){
     return Array.from(uniqueSet);
 }
 
+export default removerDuplicadas;
+
 /*const arraComDuplicatas = [1, 2, 2, 3, 3, 4, 5, 5];
 const arraySemDuplicatas = removerDuplicadas(arraComDuplicatas);
 console.log(arraySemDuplicatas);*/
