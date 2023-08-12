cabc29f52c8264daaca62914dc615274a40c578e
GabrielAraujoNeves <138191283+gabriellneeves7@users.noreply.github.com>
2023-08-11 22:32:41 -0400

    foi criado teste para função verificarAnagrama

--- Algoritmos/verificarAnagrama.js
+++ Algoritmos/verificarAnagrama.js
@@ -1,10 +1,10 @@
 function verificarAnagrama(str1, str2) {
 
-    str1 = str1.replace(/\s/g, '').toLowerCase();
-    str2 = str2.replace(/\s/g, '').toLowerCase();
+    const stri1 = str1.replace(/\s/g, '').toLowerCase();
+    const stri2 = str2.replace(/\s/g, '').toLowerCase();
   
     
-    if (str1.length !== str2.length) {
+    if (stri1.length !== stri2.length) {
       return false;
     }
   
@@ -13,11 +13,11 @@ function verificarAnagrama(str1, str2) {
     const charCount2 = {};
   
 
-    for (let char of str1) {
+    for (let char of stri1) {
       charCount1[char] = (charCount1[char] || 0) + 1;
     }
   
-    for (let char of str2) {
+    for (let char of stri2) {
       charCount2[char] = (charCount2[char] || 0) + 1;
     }
   
@@ -29,7 +29,9 @@ function verificarAnagrama(str1, str2) {
   
     return true;
   }
+
+export default verificarAnagrama;
   
-  
+  /*
   console.log(verificarAnagrama("listen", "silent"));  
-  
+  */
